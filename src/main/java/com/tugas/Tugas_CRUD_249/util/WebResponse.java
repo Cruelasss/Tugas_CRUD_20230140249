48de4d5481991032558d530ec3d2575b5c35e53d
Cruelasss <irula5198@gmail.com>
2026-03-13 13:40:59 +0700

    feat: menambahkan kelas util WebResponse sebagai standar struktur response API

---
+++ src/main/java/com/tugas/Tugas_CRUD_249/util/WebResponse.java
@@ -0,0 +1,16 @@
+package com.tugas.Tugas_CRUD_249.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class WebResponse<T> {
+    private String message;
+    private String status;
+    private T data;
+}
