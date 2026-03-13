0bfcae8a2c84c7b3ee705a252615ef87f0e5a72a
Cruelasss <irula5198@gmail.com>
2026-03-13 12:26:33 +0700

    Initial commit

---
+++ src/main/java/com/tugas/Tugas_CRUD_249/TugasCrud249Application.java
@@ -0,0 +1,13 @@
+package com.tugas.Tugas_CRUD_249;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TugasCrud249Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TugasCrud249Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/tugas/Tugas_CRUD_249/TugasCrud249ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tugas.Tugas_CRUD_249;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TugasCrud249ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
